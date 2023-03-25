16ab4a620f58b81d5665e1f207077792b066640d
我的ID被隙间了 <710871626@qq.com>
2023-03-25 15:45:16 +0800

    完善伤害体

--- Assets/Mod_Entity/Scripts/DamageEntity.cs
+++ Assets/Mod_Entity/Scripts/DamageEntity.cs
@@ -18,6 +18,10 @@ namespace Mod_Entity
         /// 计时器
         /// </summary>
         public float timer;
+        /// <summary>
+        /// 附加计数器
+        /// </summary>
+        public int times;
     }
     /// <summary>
     /// 伤害体脚本
@@ -33,13 +37,21 @@ namespace Mod_Entity
 
         #region 属性
         private object owner;
-        private float damage;//伤害值
+        private Dictionary<object, Timer> timers = new Dictionary<object, Timer>();//计时器
         private List<string> damageTag;//伤害标签
-        private bool effective = true;//伤害判定是否有效
-        private int damageTimes = 1;//对同一对象的伤害次数
+
+        private float damage;//伤害值
+        private int damageTimes = 1;//对同一对象的最大允许伤害次数
         private float cd = 0.05f;//多段伤害触发间隔
         private float power = 0;//击退力量
-        private Dictionary<object,Timer> timers= new Dictionary<object, Timer>();//计时器
+
+        private bool effective = true;//伤害判定是否有效
+        private bool autoDead = false;//自动死亡
+        private bool hitsLimit = false;//是否限定造成伤害的总次数
+        private int hits = 0;//当前伤害判定生效次数
+        private int maxHits = 1;//最大伤害生效次数
+        private float maxLiveTime = 1;//存活时间
+        
         #endregion
 
         #region 公开属性
@@ -58,13 +70,6 @@ namespace Mod_Entity
             get => damage; set => damage = value;
         }
         /// <summary>
-        /// 判定是否启用
-        /// </summary>
-        public bool Effective
-        {
-            get => effective; set => effective = value;
-        }
-        /// <summary>
         /// 伤害次数
         /// </summary>
         public int DamageTimes
@@ -78,6 +83,51 @@ namespace Mod_Entity
         {
             get => cd; set => cd = value;
         }
+        /// <summary>
+        /// 击退力度
+        /// </summary>
+        public float Power
+        {
+            get => power;set => power = value;
+        }
+        /// <summary>
+        /// 伤害体存活时间
+        /// </summary>
+        public float LiveTime
+        {
+            get => maxLiveTime; set => maxLiveTime = value;
+        }
+
+
+        /// <summary>
+        /// 判定是否启用
+        /// </summary>
+        public bool Effective
+        {
+            get => effective; set => effective = value;
+        }
+        /// <summary>
+        /// 是否在寿命尽头自动销毁此物体
+        /// </summary>
+        public bool AutoDead
+        { 
+            get => autoDead; set => autoDead = value;
+        }
+        /// <summary>
+        /// 是否限制最大伤害次数，达到最大伤害次数时自动销毁
+        /// </summary>
+        public bool HitsLimit
+        {
+            get => hitsLimit;set => hitsLimit = value;
+        }
+        /// <summary>
+        /// 最大伤害次数
+        /// </summary>
+        public int MaxHits { get => maxHits; set => maxHits = value; }
+        /// <summary>
+        /// 最大存活时间
+        /// </summary>
+        public float MaxLiveTime { get => maxLiveTime;set=> maxLiveTime = value; }
         #endregion
 
         #region 公开函数
@@ -120,18 +170,27 @@ namespace Mod_Entity
                 {
                     if(isInside(entity))//判断是否已拥有计时器
                     {
-                        if(CDok(entity))//判断伤害cd是否冷却
+                        if(Damagable(entity))//判断伤害cd是否冷却
                         {
-                            Health health = entity.GetAttribute<Health>();
+                            ATHealth health = entity.GetAttribute<ATHealth>();
                             health.ChangeHealth(damage, Owner);
                             RestartCD(health);//重置计时器
                         }
                     }
                     else
                     {
-                        Health health = entity.GetAttribute<Health>();
+                        ATHealth health = entity.GetAttribute<ATHealth>();
                         health.ChangeHealth(damage, Owner);
                         AddCdLabel(health);//添加计时器
+                    }
+                    timers[entity].times++;
+                    hits++;
+                    if(hitsLimit)//对总伤害次数有限制
+                    {
+                        if(hits >= maxHits)
+                        {
+                            Destroy(gameObject);
+                        }
                     }
                 }
             }
@@ -162,13 +221,13 @@ namespace Mod_Entity
             return false;
         }
         /// <summary>
-        /// 判断指定判定对象 伤害cd是否冷却
+        /// 判断指定判定对象 是否可进行伤害判定（主要判定cd是否冷却，是否达到最大伤害次数）
         /// </summary>
         /// <param name="aim">需要判定的对象</param>
         /// <returns></returns>
-        private bool CDok(object aim)
+        private bool Damagable(object aim)
         {
-            if (timers[aim].timer <= 0) { return true; }
+            if (timers[aim].timer <= 0 && timers[aim].times<damageTimes) { return true; }
             return false;
         }
         /// <summary>
@@ -209,6 +268,15 @@ namespace Mod_Entity
             for(int i=0;i<timers.Count;i++)
             {
                 timers[i].timer -= Time.deltaTime;
+            }
+
+            if(autoDead)
+            {
+                maxLiveTime -= Time.deltaTime;
+                if (maxLiveTime <= 0)
+                {
+                    Destroy(gameObject);
+                }
             }
         }
         #endregion
