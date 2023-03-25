080c30aa158527b81096bbc5bfa9eaab6260a20a
我的ID被隙间了 <710871626@qq.com>
2023-03-25 15:15:22 +0800

    运动行为基本模板ok

--- Assets/Mod_Behaviour/Scripts/BHMove.cs
+++ Assets/Mod_Behaviour/Scripts/BHMove.cs
@@ -6,7 +6,8 @@ using Mod_Entity;
 
 namespace Mod_Behaviour
 {
-    enum State { Waiting,Running}
+    public enum State { Waiting, Running }
+    public enum Dir {left,right,up,down }
 
     /// <summary>
     /// 移动行为
@@ -18,49 +19,71 @@ namespace Mod_Behaviour
         private Entity owner;
         private State state = State.Waiting;//状态机状态
         private Animator animator;//动画机
-        private Vector2 direction;//运动方向
+        private Dir direction;//运动方向
+        private float speed;//移动速度
         #endregion
 
         #region 公开属性
-        public string Name { get=>attributeName; set=>attributeName=value; }
+        public string Name { get => attributeName; set => attributeName = value; }
         public Entity Owner { get; set; }
-
-        public Vector2 Direction 
-        { 
-            get => direction;
-            set
-            {
-                direction = value.normalized;
-            }
-                }
-
+        /// <summary>
+        /// 运动方向
+        /// </summary>
+        public Dir Direction { get; set; }
+        /// <summary>
+        /// 移动速度
+        /// </summary>
+        public float Speed { get; set; }
         #endregion
+
         #region 功能属性
         public object GetStatus()
         {
             return state;
         }
-
         public bool StartBehaviour()
         {
+            state = State.Running;
             return true;
         }
-
         public bool StopBehaviour()
         {
-
+            state = State.Waiting;
             return true;
+        }
+        #endregion
+
+        #region 内部函数
+        private void Running()
+        {
+            switch(direction)
+            {
+                case Dir.left:
+                    owner.transform.position -= new Vector3(speed,0,0);
+                    break;
+                case Dir.right:
+                    owner.transform.position += new Vector3(speed,0,0);
+                    break;
+            }
         }
         #endregion
 
         #region Unity
         private void Start()
         {
-            
+
         }
         private void Update()
         {
-            
+            switch (state)
+            {
+                case State.Running:
+                    Running();
+                    break;
+                case State.Waiting:
+
+                    break;
+            }
         }
         #endregion
     }
